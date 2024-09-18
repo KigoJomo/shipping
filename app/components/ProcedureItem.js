9a95abf881e182b602198efd347f50fd128518cd
KigoJomo <kigojomo@gmail.com>
2024-09-18 20:12:19 +0300

    Palette Update

--- app/components/ProcedureItem.js
+++ app/components/ProcedureItem.js
@@ -8,22 +8,22 @@ const ProcedureItem = ({ index, title, description, isActive, onClick }) => {
       duration={1 + (index * 0.25)}
       className={`w-full flex flex-col items-center ${
         isActive ? "max-h-36 md:max-h-32" : "max-h-16"
-      } px-4 bg-white overflow-hidden rounded-2xl transition-all duration-500`}
+      } px-4 bg-tertiary overflow-hidden rounded-2xl transition-all duration-500`}
     >
       <button
         className="title w-full flex items-center gap-4 h-16 flex-shrink-0 relative"
         onClick={onClick}
       >
-        <h3 className="text-black text-lg font-light">0{index + 1}</h3>
-        <h3 className="text-black text-lg font-medium">{title}</h3>
-        <h3 className="text-black text-5xl font-extralight absolute right-0">
+        <h3 className=" text-lg font-light">0{index + 1}</h3>
+        <h3 className=" text-lg font-medium">{title}</h3>
+        <h3 className=" text-5xl font-extralight absolute right-0">
           {isActive ? <>-</> : <>+</>}
         </h3>
       </button>
       <div
         className="description w-full h-20 md:h-16 pl-9 pr-2 text-sm"
       >
-        <p className="max-w-full text-primary-dark text-xs md:text-sm">{description}</p>
+        <p className="max-w-full text-white text-xs md:text-sm">{description}</p>
       </div>
     </ScrollAnimationWrapper>
   );
