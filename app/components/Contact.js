14a6627dc27bcb2d9c63d13b6907a9e325a9dd5e
KigoJomo <kigojomo@gmail.com>
2024-09-22 21:41:55 +0300

    Update

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 const Contact = () => {
   return (
     <section className="bg flex flex-col gap-8 relative overflow-y-scroll md:overflow-hidden h-fit md:h-screen">
-      <div className="w-full h-fit md:h-[90%] flex flex-col md:flex-row gap-8">
+      <div className="w-full h-fit flex flex-col md:flex-row gap-8">
         <Addresses />
         <ContactForm />
       </div>
