c580879b212c30aa7479bb1b3fdeeee1edc2d56e
KigoJomo <kigojomo@gmail.com>
2024-09-22 22:00:51 +0300

    Update

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <section className="bg flex flex-col items-center gap-8 relative overflow-y-scroll md:overflow-hidden h-fit pb-0">
 
-      <div className="w-full h-fit flex flex-col gap-8">
+      <div className="w-full h-fit flex flex-col gap-8 md:gap-24">
         <Addresses />
         <ContactForm />
       </div>
