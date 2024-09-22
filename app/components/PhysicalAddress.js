0695839a7c0308663b6812218edb990debbd56d7
KigoJomo <kigojomo@gmail.com>
2024-09-22 21:59:35 +0300

    Update

--- app/components/PhysicalAddress.js
+++ app/components/PhysicalAddress.js
@@ -11,7 +11,7 @@ const WarehouseDetail = ({label, text}) =>{
 
 const PhysicalAddress = ({ title, city, address, zip, state, phone_number})=>{
     return (
-        <div className={`w-full md:w-64 p-4 flex flex-col gap-6 bg-tertiary rounded-3xl`}>
+        <div className={`w-full p-4 flex flex-col gap-6 bg-tertiary rounded-3xl`}>
             <div className="w-full flex items-center gap-4 pb-2 border-b border-accent border-opacity-5">
                 <WarehouseIcon />
                 <h4 className="font-bold">{title}</h4>
